4dae60ff4dfc64a0a095fd9f903615b7a8694e15
Denisson-Silva <denisson.boulevardmotos@gmail.com>
2021-05-14 20:57:31 -0300

    inicio do projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const Raiz = styled.div`
+display: grid;
+grid-template-columns: 1fr 3fr 1fr;
+padding: 18px;
+gap: 6px;
+heigth: 100vh;
+`
+
+const Filtros = styled.div`
+border: solid 1px black;
+height: 100vh;
+`
+const Produtos = styled.div`
+border: solid 1px black;
+`
+const Carrinho = styled.div`
+border: solid 1px black;
+`
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Raiz>
+      <Filtros>
+        filtro
+      </Filtros>
+
+      <Produtos>
+        Produtos
+      </Produtos>
+
+      <Carrinho>
+        Carrinho
+      </Carrinho>
+      
+    </Raiz>
   );
 }
 
