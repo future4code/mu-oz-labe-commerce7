21e051539214a73412b20e853bf583d4d4af0598
Denisson-Silva <denisson.boulevardmotos@gmail.com>
2021-05-22 21:30:46 -0300

    agora vai

--- src/App.js
+++ src/App.js
@@ -132,7 +132,7 @@ export default class App extends React.Component {
     this.setState({nameFiltro: event.target.value})
   }
 
-  onFilter = () => {
+  /* onFilter = () => {
     const listaFiltrada = this.state.imagens.map((filtro) => {
       if(filtro.value < minFilter){
         return {
@@ -143,7 +143,7 @@ export default class App extends React.Component {
       }
          
       
-    })
+    }) */
      
 
    
